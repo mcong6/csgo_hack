56192caa7a2fe5f675112ff3d7d69d8958a8658f
AWPC <cc@gmail.com>
2020-06-20 18:36:52 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,50 @@
+import json
+import os
+import pymem
+from path import HAZEDUMPER_PATH
+
+with open(os.path.join(HAZEDUMPER_PATH, "csgo.json"), 'r') as f:
+    offsets_dict = json.load(f)
+
+signatures = offsets_dict['signatures']
+netvars = offsets_dict["netvars"]
+dwEntityList = signatures["dwEntityList"]
+dwGlowObjectManager = signatures["dwGlowObjectManager"]
+m_iGlowIndex = netvars["m_iGlowIndex"]
+m_iTeamNum = netvars["m_iTeamNum"]
+
+
+def main():
+    print("Diamond has launched.")
+    pm = pymem.Pymem("csgo.exe")
+    client = pymem.process.module_from_name(pm.process_handle, "client.dll").lpBaseOfDll
+
+    while True:
+        glow_manager = pm.read_int(client + dwGlowObjectManager)
+
+        for i in range(1, 32):  # Entities 1-32 are reserved for players.
+            entity = pm.read_int(client + dwEntityList + i * 0x10)
+
+            if entity:
+                entity_team_id = pm.read_int(entity + m_iTeamNum)
+                entity_glow = pm.read_int(entity + m_iGlowIndex)
+
+                if entity_team_id == 2:  # Terrorist
+                    pm.write_float(glow_manager + entity_glow * 0x38 + 0x4, float(1))   # R
+                    pm.write_float(glow_manager + entity_glow * 0x38 + 0x8, float(0))   # G
+                    pm.write_float(glow_manager + entity_glow * 0x38 + 0xC, float(0))   # B
+                    pm.write_float(glow_manager + entity_glow * 0x38 + 0x10, float(1))  # Alpha
+                    pm.write_int(glow_manager + entity_glow * 0x38 + 0x24, 1)           # Enable glow
+
+                elif entity_team_id == 3:  # Counter-terrorist
+                    pm.write_float(glow_manager + entity_glow * 0x38 + 0x4, float(0))   # R
+                    pm.write_float(glow_manager + entity_glow * 0x38 + 0x8, float(0))   # G
+                    pm.write_float(glow_manager + entity_glow * 0x38 + 0xC, float(1))   # B
+                    pm.write_float(glow_manager + entity_glow * 0x38 + 0x10, float(1))  # Alpha
+                    pm.write_int(glow_manager + entity_glow * 0x38 + 0x24, 1)           # Enable glow
+
+
+if __name__ == '__main__':
+    main()
+
+
---
+++ path.py
@@ -0,0 +1,4 @@
+import os
+
+CSGO_PATH = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
+HAZEDUMPER_PATH = os.path.join(CSGO_PATH, "hazedumper/")
